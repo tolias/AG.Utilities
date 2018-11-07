7b6ea98cdfc0f578c72e7e0bcfc38611698db559
Anatolii Gumennyi <anatol.humenniy@gmail.com>
2018-11-07 15:52:09 +0200

    Removed wrong project refs. Created nuget package

--- AG.Utilities/Properties/AssemblyInfo.cs
+++ AG.Utilities/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AG.Utilities")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Miscellaneous .NET helpers for common needs")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AG")]
 [assembly: AssemblyProduct("AG.Utilities")]
-[assembly: AssemblyCopyright("Copyright © AG 2015")]
+[assembly: AssemblyCopyright("Copyright © AG 2015-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
